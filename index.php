9d3a493e6da66db8d362d940fb3afed60864ef91
Hayao Midoriya <zs6096@gmail.com>
2019-12-07 14:22:18 +0800

    Core: 引入ThinkPHP框架

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  舞萌DX店铺分布可视化地图
+ *
+ *  @author Hayao Midoriya <zs6096@gmail.com>
+ *  @license WTFPL
+ *  @description 入口文件
+ */
+
+// 检测PHP环境
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+    die('PHP版本过低，请更新PHP至5.3.0以上版本');
+}
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG', true);
+
+// 绑定模块
+define('BIND_MODULE', 'Map');
+
+// 定义应用目录
+define('APP_PATH', './App/');
+
+// 引入ThinkPHP入口文件
+require './Framework/ThinkPHP.php';

3fe518f90f54b19b2a4aa417802adf73d686bc4b
Hayao Midoriya <zs6096@gmail.com>
2019-12-08 17:38:03 +0800

    DB: 添加店铺位置纠偏表

---
+++ Database/location-fix-data.sql
@@ -0,0 +1,10 @@
+/**
+ *  舞萌DX店铺分布可视化地图
+ *
+ *  @author Hayao Midoriya <zs6096@gmail.com>
+ *  @license WTFPL
+ *  @description 店铺地理位置纠偏数据表
+ */
+
+INSERT INTO maidx_location_fix (shop_id, shop_location) VALUES
+(1113, '{"lng": "113.547394", "lat": "22.241392"}');

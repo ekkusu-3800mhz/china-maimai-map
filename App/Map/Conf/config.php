149760926a22300e7a25244bcc57f3df94193230
Hayao Midoriya <zs6096@gmail.com>
2019-12-08 17:39:19 +0800

    App: 添加静态路由表

---
+++ App/Map/Conf/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  舞萌DX店铺分布可视化地图
+ *
+ *  @author Hayao Midoriya <zs6096@gmail.com>
+ *  @license WTFPL
+ *  @description Map模块静态路由表
+ */
+
+return array(
+    'URL_ROUTER_ON' => true,
+    'URL_MAP_RULES' => array(
+        'index'     => 'Page/map',
+        'api/query' => 'Api/query'
+    )
+);

1aed80bee618a2925846e96844b32e8475bad917
Hayao Midoriya <zs6096@gmail.com>
2019-12-08 17:40:26 +0800

    App: 添加页面控制器类

---
+++ App/Map/Controller/PageController.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *  舞萌DX店铺分布可视化地图
+ *
+ *  @author Hayao Midoriya <zs6096@gmail.com>
+ *  @license WTFPL
+ *  @description 页面控制器类
+ *  @package Map\Controller
+ */
+
+namespace Map\Controller;
+
+use \Map\Lib\Controller\Webpage,
+    \Think\Exception;
+
+class PageController extends Webpage {
+
+    /**
+     *  地图首页操作
+     *
+     *  @return void
+     */
+
+    public function mapAction() {
+        $this->page('get', function() {
+            $this->assign('apiUrl', BASE_URL);
+            $this->display();
+        });
+    }
+
+}

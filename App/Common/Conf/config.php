b95b152ff7c7412df55fdaa5eca840f2c14cfd80
Hayao Midoriya <zs6096@gmail.com>
2020-03-25 04:15:45 +0800

    [!] 添加对MySQL数据源和数据缓存配置的引用

--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -11,6 +11,27 @@ $config = require_once SITE_ROOT . DIRECTORY_SEPARATOR . 'config.php';
 
 return array(
 
+    /* MySQL数据源配置 */
+
+    'DB_TYPE'              => 'mysql',
+    'DB_HOST'              => $config['datasource']['mysql']['host'],
+    'DB_PORT'              => $config['datasource']['mysql']['port'],
+    'DB_USER'              => $config['datasource']['mysql']['user'],
+    'DB_PWD'               => $config['datasource']['mysql']['password'],
+    'DB_NAME'              => $config['datasource']['mysql']['database'],
+    'DB_PREFIX'            => $config['datasource']['mysql']['prefix'],
+    'DB_CHARSET'           => 'utf8',
+
+    /* 数据缓存配置 */
+
+    'DATA_CACHE_TYPE'      => 'Redis',
+    'REDIS_HOST'           => $config['cache']['redis']['host'],
+    'REDIS_PORT'           => $config['cache']['redis']['port'],
+    'REDIS_AUTH'           => $config['cache']['redis']['password'],
+    'REDIS_TIMEOUT'        => '300',
+    'DATA_CACHE_PREFIX'    => $config['cache']['redis']['prefix'],
+    'DATA_CACHE_TIME'      => $config['cache']['redis']['expires'],
+
     /* 接口地址配置 */
 
     'API_URL'              => $config['dxservice']['url'],
